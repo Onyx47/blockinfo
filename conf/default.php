3a9af6cd394a69fa5eb4504a9c22ce418e71fddd
Bojan Nemcic <bnemcic@gmail.com>
2013-05-28 11:56:16 +0200

    Fix indentation issues

--- conf/default.php
+++ conf/default.php
@@ -5,22 +5,22 @@
  * @author Bojan Nemcic <bnemcic@gmail.com>
  */
 
-$conf['attr_uppercase'] 	= 'words';	// Automatically capitalize attribute names
-										// 'none': no change, leave attribute name as defined in tag
-										// 'first': capitalize first word, leave the rest as-is
-										// 'words': capitalize each word
-										// 'all': all caps
+$conf['attr_uppercase']     = 'words';  // Automatically capitalize attribute names
+                                        // 'none': no change, leave attribute name as defined in tag
+                                        // 'first': capitalize first word, leave the rest as-is
+                                        // 'words': capitalize each word
+                                        // 'all': all caps
 
-$conf['value_uppercase'] 	= 'none';	// Automatically capitalize values
-										// 'none': no change, leave attribute name as defined in tag
-										// 'first': capitalize first word, leave the rest as-is
-										// 'words': capitalize each word
-										// 'all': all caps
-										// NOTE: only applicable if value_parse is false
-										//       or value_format_force is true
+$conf['value_uppercase']    = 'none';   // Automatically capitalize values
+                                        // 'none': no change, leave attribute name as defined in tag
+                                        // 'first': capitalize first word, leave the rest as-is
+                                        // 'words': capitalize each word
+                                        // 'all': all caps
+                                        // NOTE: only applicable if value_parse is false
+                                        //       or value_format_force is true
 
-$conf['value_parse']		= true;		// If true, values will be parsed as dokuwiki syntax
+$conf['value_parse']        = true;     // If true, values will be parsed as dokuwiki syntax
 
-$conf['value_format_force']	= false;	// Force plugin to format values
-										// even if they get parsed as dokuwiki syntax
+$conf['value_format_force'] = false;    // Force plugin to format values
+                                        // even if they get parsed as dokuwiki syntax
 
