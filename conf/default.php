34bb31d9d7de40bf46b6cb846c9e907d58ffafbb
Bojan Nemcic <bnemcic@gmail.com>
2013-06-05 21:12:29 +0200

    Nuked unecessary settings

--- conf/default.php
+++ conf/default.php
@@ -5,29 +5,7 @@
  * @author Bojan Nemcic <bnemcic@gmail.com>
  */
 
-/*
- * STYLING OPTIONS
- */
 
-$conf['box_border_width']       = 5;            /* Block info box border width, in pixels*/
-$conf['box_sizing_policy']      = 'max-width';  /* Box sizing policy.
-                                                        'min-width': set minimum width to the given value
-                                                        'max-width': set maximum width to the given value
-                                                        'width':     set fixed width to the given value */
-$conf['box_size']               = '250';        /* Box width, in pixels */
-
-
-/* Set any of the following color settings to a valid hex color code
-   like #ff0000 (or #f00) OR a valid CSS color name if you want to set a custom color.
-   If set to blank or 'inherit' wiki template border styles will be used */
-
-$conf['box_border_color']       = 'inherit';    // Block info box border color
-$conf['title_background']       = 'inherit';    // Block info title background color
-$conf['title_color']            = 'inherit';    // Block info title text color
-$conf['attr_background']        = 'inherit';    // Attribute name background color
-$conf['attr_color']             = 'inherit';    // Attribute name text color
-$conf['value_background']       = 'inherit';    // Value background color
-$conf['value_color']            = 'inherit';    // Value text color
 /*
  * FORMATTING OPTIONS
  */
@@ -35,7 +13,7 @@ $conf['attr_uppercase']         = 'words';      // Automatically capitalize attr
                                                 // 'none': no change, leave attribute name as defined in tag
                                                 // 'first': capitalize first word, leave the rest as-is
                                                 // 'words': capitalize each word
-// 'all': all caps
+                                                // 'all': all caps
 
 $conf['attr_parse']             = false;        // If true, attributes will be parsed as dokuwiki syntax
 $conf['attr_format_force']      = false;        // Force plugin to format attributes
